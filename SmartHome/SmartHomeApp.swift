c93516fe7e55ff2062d663a180276a63160c9e23
fbara <fbara1@yahoo.com>
2020-10-16 20:00:13 -0500

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Frank Bara on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
